aeb083a5d8f066fdc6976ee083b1875027419e9a
Denis Rasulev <ranklord@gmail.com>
2017-12-28 20:02:56 +0100

    init

---
+++ README.md
@@ -0,0 +1,27 @@
+# **Text Tool** #
+
+## Project for app to clean and tokenize texts ##
+
+### Here are some basic ideas: ###
+
+* Get text through interface window (copy-paste) or via file upload
+
+* Clean it using recommended settings and/or user selected things to clean from
+
+* Analyze and show uploaded text analytics
+
+* Create N-grams
+
+* Payment?
+
+* Links to download
+
+### What to work on: ###
+
+1. Text processing
+
+2. Interface
+
+3. Domain
+
+4. Platform
---
+++ textor.py
@@ -0,0 +1,92 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+Project to create text cleaning script to clean text from user selected things.
+
+- Called without any parameters it will show help
+- Called with file input name only, it will show file stats, i.e. file size, number of words,
+  and clean from everything and save lowercase words list in csv format.
+- Rest is according to the parameters
+
+1. Open file
+2. Get / show / save file stats
+3. User selected options for cleaning: either by args / options or via GUI
+4. Process text
+5. Save output in user selected format: plain text, csv, ?
+"""
+
+# import required modules
+import os
+import sys
+import argparse
+
+
+# colors class to use in outputs
+class Color:
+    HEAD = '\033[95m'
+    OKBL = '\033[94m'
+    OKGR = '\033[92m'
+    FAIL = '\033[93m'
+    WARN = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDR = '\033[4m'
+
+
+# function to check file existence
+def check_if_exists(file_name):
+    """Check if output file already exists and propose to append to it or overwrite it."""
+    # global variable to know if we shall append or overwrite output file
+    global output_file_overwrite
+
+    # check if file already exists and if yes, ask what user wants to do with it - append or overwrite
+    if os.path.exists(file_name):
+        answer = input("Output file already exists. (A)ppend (default) to it or (O)verwrite it? ")
+
+        # if user wants to overwrite file, give notice and set global variable to True
+        if answer in ['o', 'O', '0']:
+            print(Color.WARN + "Output file will be overwritten!" + Color.ENDC)
+            output_file_overwrite = True
+
+        # if user wants to append file, give notice and set global variable to False
+        elif answer in ['a', 'A', '']:
+            print("Output file will be appended.")
+            output_file_overwrite = False
+
+        # if user entered anything else, give notice and exit
+        else:
+            print(Color.FAIL + "Please, enter 'A' to append information to the output file or 'O' to overwrite it."
+                  + Color.ENDC)
+            sys.exit(0)
+
+    # if file does not exist then set global variable to True since new file will be created
+    else:
+        output_file_overwrite = True
+
+    # return file name for further processing
+    return file_name
+
+
+# create args parser and add possible arguments
+parser = argparse.ArgumentParser(
+    prog='textor',
+    description="Script to clean source text from all and/or user selected elements.",
+    epilog="textor v0.1. ©2017 Denis Rasulev. All Rights Reserved.")
+parser.add_argument('-i', '--input', type=argparse.FileType('r', encoding='UTF-8'), help="source text file")
+parser.add_argument('-o', '--output', type=check_if_exists, help="output text file")
+parser.add_argument('-s', '--settings', help="settings on what to clean from source")
+
+# parse arguments
+args = parser.parse_args()
+
+#
+print('\n')
+print("Input: {}".format(os.path.abspath(args.input.name)))
+print("Output: {}".format(args.output))
+print("Settings: {}".format(args.settings))
+print("Overwrite: {}".format(output_file_overwrite))
+print('\n')
+
+source_text = args.input.read()
+print(source_text)
