6ac60cbec4fe338633efb04a2fa4b87e9237f403
Denis Rasulev <ranklord@gmail.com>
2018-01-04 17:52:05 +0100

    cleanup

--- tp.py
+++ tp.py
@@ -2,11 +2,12 @@
 # -*- coding: utf-8 -*-
 
 """
-Project to create text cleaning script to clean text from user selected things.
+Script to clean text from all or user selected things.
 
 - Called without any parameters it will show help
-- Called with file input name only, it will show file stats, i.e. file size, number of words,
-  and clean from everything and save lowercase words list in csv format.
+- Called with file input name only, it will show file stats, i.e. file size,
+  number of words, and clean from everything and save lower case words list
+  in csv format.
 - Rest is according to the parameters
 
 1. Open file
@@ -19,41 +20,44 @@ Project to create text cleaning script to clean text from user selected things.
 # import required modules
 import os
 import sys
-import helper
+import time
 import argparse
 import chardet
+import helper
 
 
-# class to define colors for outputs, defines colors as escape sequences
+# class to define colors for messages, defines colors as escape sequences
 class Color:
+    """Class defines colors for messages"""
+
     def __init__(self):
-        self.HEAD = '\033[95m'
-        self.OKBL = '\033[94m'
         self.OKGR = '\033[92m'
         self.FAIL = '\033[93m'
-        self.WARN = '\033[91m'
+        # self.WARN = '\033[91m'
+        self.WARN = '\033[33m'
         self.ENDC = '\033[0m'
         self.BOLD = '\033[1m'
-        self.UNDR = '\033[4m'
 
 
-# create instance
-clr = Color()
+# create message colors instance
+color = Color()
 
 
 # function to check if file exists
 def check_if_exists(file_name):
-    """Check if output file name already exists and then propose to append to it or overwrite it."""
+    """Check if output file name already exists and then propose to append to it
+    or overwrite it."""
     # global variable to know if we shall append or overwrite output file
     global output_file_overwrite
 
-    # check if file already exists and if yes, ask what user wants to do with it - append or overwrite
+    # check if file already exists and if yes, ask what user wants to do with it
+    # - append or overwrite
     if os.path.exists(file_name):
-        answer = input("File '" + file_name + "' already exists. (A)ppend (default) to it or (O)verwrite it? ")
+        answer = input("File '" + file_name + "' already exists.\n(A)ppend (default) to it or (O)verwrite it? ")
 
         # if user wants to overwrite file, give notice and set global variable to True
         if answer in ['o', 'O', '0']:
-            print(clr.WARN + "ATTENTION: File will be overwritten!" + clr.ENDC)
+            print(color.WARN + "ATTENTION: File will be overwritten!" + color.ENDC)
             output_file_overwrite = True
 
         # if user wants to append file, give notice and set global variable to False
@@ -63,8 +67,8 @@ def check_if_exists(file_name):
 
         # if user enters anything else, give help notice and exit
         else:
-            print(clr.FAIL + "Please, enter 'A' to append information to the output file or 'O' to overwrite it."
-                  + clr.ENDC)
+            print(color.FAIL + "Please, enter 'A' to append information to the output file or 'O' to overwrite it."
+                  + color.ENDC)
             sys.exit(0)
 
     # if file does not exist then set global variable to True since new file will be created
@@ -77,11 +81,13 @@ def check_if_exists(file_name):
 
 # description of what the script does
 description = """
-Script shows this help message and exits if called without any arguments or with optional '-h', '--help'.
-Given one parameter, the script assumes it is a SOURCE and attempts to read it for further processing.
+Script shows this help message and exits if called without any arguments
+or with optional '-h', '--help'. Given one parameter, the script assumes
+it is a SOURCE and attempts to read it for further processing.
 
-Script cleans SOURCE from all (default) or only user selected elements and saves the result into a file.
-By default it saves result as comma-separated list of words in lower case.
+Script cleans SOURCE from all (default) or only user selected elements and
+saves the result into a file. By default it saves result as comma-separated
+list of words in lower case.
 
 These are elements removed from a source by default:
 - punctuation
@@ -94,8 +100,9 @@ Some extra work done by script:
 - spelling correction, including common abbreviations
 - non-unicode characters are converted to unicode analogs
 
-If output file name was not specified, the result is saved to .csv file named as SOURCE with appended '_cleaned' word.
-For instance, if you called the script as 'tp.py text.txt' then the result will be saved to 'text_cleaned.csv' file.
+If output file name was not specified, the result is saved to .csv file
+named as SOURCE with appended '_cleaned' word. For instance, if you called
+'tp.py text.txt' then the result will be saved to 'text_cleaned.csv' file.
 """
 
 # create arguments parser
@@ -105,11 +112,11 @@ parser = argparse.ArgumentParser(
     usage='%(prog)s source [-o output] [-f format]',
     formatter_class=argparse.RawDescriptionHelpFormatter,
     description=description,
-    epilog="Text Processor - ver 0.1 © 2017 Denis Rasulev. All Rights Reserved.")
+    epilog="Text Processor. Ver 0.1 ©2017 Denis Rasulev. All Rights Reserved.")
 
 # add arguments
 parser.add_argument('ifile',
-                    metavar='SOURCE', type=argparse.FileType('r'),
+                    metavar='SOURCE',  # type=argparse.FileType('r'),
                     help="source to be processed")
 parser.add_argument('-o', '--ofile',
                     metavar='OUTPUT', type=check_if_exists,
@@ -120,31 +127,48 @@ parser.add_argument('-f', '--format',
                     help="specifies output file format: csv or txt")
 
 
-# if script started without any arguments, print help and exit
-if len(sys.argv) < 2:
+# if script called without any arguments, print short usage note and exit
+if len(sys.argv) == 1:
+    print("usage: tp.py source [-o output] [-f format]")
+    print("help : tp.py -h, --help")
+    sys.exit(0)
+
+# if script called with any argument requesting help, print help and exit
+if sys.argv[1] in ['?', '/?', '-h', '--help']:
     parser.print_help()
     sys.exit(0)
 
 
-# if one or more arguments were provided, parse arguments
+# if any other arguments were provided, parse them
 args = parser.parse_args()
 
-# TODO: check if http is in the source name and create getter for it
+# TODO: processor for HTTP URLs
 # check if user provided URL as a SOURCE
-if args.ifile.name.startswith('http'):
-    import urllib.request
-    input_file = urllib.request.urlopen(args.ifile.name).read()
-else:
-    input_file = open(args.ifile.name, 'rb').read()
+if args.ifile.startswith('http'):
 
-# detect input file encoding
-f_encoding = chardet.detect(input_file)['encoding']
+    # import requests
+    # r = requests.get(args.ifile)
+    # print(r.status_code)
+    # print(r.headers['content-type'])
+    # print(r.encoding)
+    # print(r.text)
+
+    print("URLs will be accepted and processed in the future versions.")
+    sys.exit(0)
+else:
+    input_file = open(args.ifile, 'rb').read()
+    f_encoding = chardet.detect(input_file)['encoding']
+
+
+if args.ofile is None:
+    args.ofile = os.path.splitext(args.ifile)[0] + "_cleaned.csv"  # + os.path.splitext(args.ifile)[1]
+    check_if_exists(args.ofile)
 
 
 # print args
 print('\n')
-print("Input file path     : {}".format(os.path.abspath(args.ifile.name)))
-print("Input file size     : {}".format(helper.sizeof_fmt(os.path.getsize(args.ifile.name))))
+print("Input file path     : {}".format(os.path.abspath(args.ifile)))
+print("Input file size     : {}".format(helper.sizeof_fmt(os.path.getsize(args.ifile))))
 print("Input file encoding : {}".format(f_encoding))
 print("Output file path    : {}".format(os.path.abspath(args.ofile)))
 print("Output file format  : {}".format(args.format))
@@ -152,9 +176,15 @@ print("Overwrite flag      : {}".format(output_file_overwrite))
 print('\n')
 
 # main part goes here - output list of words only
-source_text = args.ifile.read()
-cleaned_text = helper.remove_punctuation_all(source_text)
+f = open(args.ifile, 'r')
+source_text = f.read()
 
+start = time.time()
+print("Pricessing started at " + str(start))
+cleaned_text = helper.remove_punctuation_all(source_text)
+end = time.time()
+print("Pricessing ended at " + str(end))
+print("Duration: " + str(end - start))
 
 # if we only need unique words
 if False:
