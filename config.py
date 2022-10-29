fbebefd9f74188efc85f3f924b233ff43f533ab5
Denis Rasulev <ranklord@gmail.com>
2022-10-29 08:36:24 +0200

    Replace static key with random dynamic

--- config.py
+++ config.py
@@ -3,9 +3,8 @@ import os
 
 class Config(object):
     APP_ROUTE = os.path.dirname(os.path.abspath(__file__))
-    SECRET_KEY = os.environ.get('SECRET_KEY') or '53afee03b75e8fb022c88fba922bb22e'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or os.urandom(32)
     SESSION_TYPE = 'filesystem'
     UPLOAD_FOLDER = '/texts'
     ALLOWED_EXTENSIONS = {'txt'}
     MAX_CONTENT_LENGTH = 1 * 1024 * 1024  # Max upload size 1MB
-
